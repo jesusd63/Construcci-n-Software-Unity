634085652aacc484f7cfe7f2083c543d7a978f0a
jesusd63 <89026878+jesusd63@users.noreply.github.com>
2023-02-17 08:49:32 -0600

    push inicial

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,35 @@
+//Estamos usando .NET
+//aquí “importamos” namespaces
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    //ciclo de vida / lifecycle
+	// - existen métodos que se invocan en momentos específicos de la vida del script
+    // Start is called before the first frame update
+    void Awake(){
+
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate(){
+
+    }
+
+    void LateUpdate(){
+        
+    }
+}
